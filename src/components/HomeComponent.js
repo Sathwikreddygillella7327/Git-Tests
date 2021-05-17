72460afd9cc7ba498f820197eb30eb7c6e8b281a
Sathwikreddygillella7327 <gillellar.ug19.cs@nitp.ac.in>
2021-05-17 16:48:29 +0530

    router commit

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { renderIntoDocument } from 'react-dom/test-utils';
+function Home(props){
+    return(
+<div className="container">
+    <h4>Home</h4>
+</div>
+    )
+}
+export default Home;
